f9995fa67622cd1757c8544c1e04954f93551b56
Henrique <henricke.ss@gmail.com>
2014-02-03 13:15:18 -0200

    initial commit Feito em Bash

--- src/br/ifes/edu/ifes/poo1/app/CadastroContatoArquivo.java
+++ src/br/ifes/edu/ifes/poo1/app/CadastroContatoArquivo.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cadastrocontatoarquivo;
+package br.ifes.edu.ifes.poo1.app;
 
 /**
  *
---
+++ src/br/ifes/edu/poo1/cgt/Controle.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ifes.edu.poo1.cgt;
+
+/**
+ *
+ * @author Henrique
+ */
+public class Controle {
+    private int t;
+}
